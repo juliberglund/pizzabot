368a3181b863c7eabe13be2d943df00ca1415f6b
Julia Berglund <jullano9@hotmail.com>
2024-11-03 19:34:00 +0100

    next step: make functions

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,24 +1,46 @@
 const vegetarian = "Vegetarian Pizza";
 const hawaiian = "Hawaiian Pizza";
 const pepperoni = "Pepperoni Pizza";
-
 const pizzaPrice = 80;
 
-//Put your Javscript code here:
-
+// Alert the menu
 alert(
-  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`
+  `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`
 );
 
-let orderName = prompt("Enter the name of the pizza you want to order today.");
+// Loop for pizza ordering
+let orderName; // Declare outside the loop
 
-let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+while (true) {
+  // Infinite loop
+  orderName = prompt(
+    `What type of pizza do you want? ${vegetarian}, ${hawaiian}, ${pepperoni}`
+  );
 
-// orderTotal = orderQuantity + pizzaPrice;
-function multiplyl(a, b) {
-  return a * b;
+  if (
+    orderName === vegetarian ||
+    orderName === hawaiian ||
+    orderName === pepperoni
+  ) {
+    let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+    let cookingTime;
+    if (orderQuantity >= 1 && orderQuantity <= 2) {
+      cookingTime = 10;
+    } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+      cookingTime = 15;
+    } else if (orderQuantity >= 6) {
+      cookingTime = 20;
+    }
+    function multiplyl(a, b) {
+      return a * b;
+    }
+    const orderTotal = multiplyl(orderQuantity, pizzaPrice);
+    alert(
+      `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take ${cookingTime} minutes.`
+    );
+    break; // Exit the loop after a valid order
+  } else {
+    alert("You have not entered the correct type of pizza. Please try again.");
+    // The loop will continue, prompting the user again
+  }
 }
-const orderTotal = multiplyl(orderQuantity, pizzaPrice);
-alert(
-  `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr`
-);
