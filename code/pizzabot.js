c51799a7dac16f774dca34d2283a7441db53f0a2
Julia Berglund <jullano9@hotmail.com>
2024-11-05 10:25:31 +0100

    adding function

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -3,44 +3,42 @@ const hawaiian = "Hawaiian Pizza";
 const pepperoni = "Pepperoni Pizza";
 const pizzaPrice = 80;
 
-// Alert the menu
+// function to see if the ordername input is correct
+const checkOrderName = (orderName) => {
+  return (
+    orderName === vegetarian.toLowerCase() ||
+    orderName === hawaiian.toLowerCase() ||
+    orderName === pepperoni.toLowerCase()
+  );
+};
+// Calculates the cost of the order
+const totalCost = (orderQuantity) => {
+  return orderQuantity * pizzaPrice;
+};
+// Calculate cookling time
+const cookingTime = (orderQuantity) => {
+  if (orderQuantity >= 1 && orderQuantity <= 2) {
+    return 10;
+  } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+    return 15;
+  } else if (orderQuantity >= 6) {
+    return 20;
+  }
+};
+// Call the functions
 alert(
   `Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian}, and ${pepperoni}.`
 );
-
-// Loop for pizza ordering
-let orderName; // Declare outside the loop
-
-while (true) {
-  // Infinite loop
-  orderName = prompt(
-    `What type of pizza do you want? ${vegetarian}, ${hawaiian}, ${pepperoni}`
+const orderName = prompt(
+  `What type of pizza do you want? ${vegetarian}, ${hawaiian}, ${pepperoni}`
+).toLowerCase();
+if (checkOrderName(orderName)) {
+  const orderQuantity = prompt(`How many of ${orderName} do you want?`);
+  const sum = totalCost(orderQuantity);
+  const time = cookingTime(orderQuantity);
+  alert(
+    `Great, I'll get started on your ${orderName} right away, it will cost ${sum} kr. The pizzas will take ${time} minutes.`
   );
-
-  if (
-    orderName === vegetarian ||
-    orderName === hawaiian ||
-    orderName === pepperoni
-  ) {
-    let orderQuantity = prompt(`How many of ${orderName} do you want?`);
-    let cookingTime;
-    if (orderQuantity >= 1 && orderQuantity <= 2) {
-      cookingTime = 10;
-    } else if (orderQuantity >= 3 && orderQuantity <= 5) {
-      cookingTime = 15;
-    } else if (orderQuantity >= 6) {
-      cookingTime = 20;
-    }
-    function multiplyl(a, b) {
-      return a * b;
-    }
-    const orderTotal = multiplyl(orderQuantity, pizzaPrice);
-    alert(
-      `Great, I'll get started on your ${orderName} right away, it will cost ${orderTotal} kr. The pizzas will take ${cookingTime} minutes.`
-    );
-    break; // Exit the loop after a valid order
-  } else {
-    alert("You have not entered the correct type of pizza. Please try again.");
-    // The loop will continue, prompting the user again
-  }
+} else {
+  alert("error");
 }
